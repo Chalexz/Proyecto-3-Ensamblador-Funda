42a500750f84e829ef8b755e0121182bd7d12f00
Alexander Alberto Wing Rojas <alexwingtablet@gmail.com>
2025-05-28 04:34:14 -0600

    Add files via upload

--- main.asm
+++ main.asm
@@ -1,32 +1,238 @@
-extern printf
+;SUPER PROYECTO ASM "Skanes and Dellar"
+;
+;Antony
+;Brandon 
+;Alex ╭∩╮(-_-)╭∩╮
+;
+;=======================================#
+;             ____
+;            / . .\
+;            \  ---<
+;             \  /
+;   __________/ /
+;-=:___________/
+;                          ╬═╬
+;                          ╬═╬
+;                          ╬═╬
+;                          ╬═╬
+;                          ╬═╬
+;
+;========================================#
 
-section .data
-    hello_world db 'Hello World!', 10, 10, 0
-    output_msg db 'The result is: %d', 10, 0
+personaje_seleccionado db 0 ; Personaje que le toca jugar
 
-section .text
+movimiento db 0
 
-global main
+;variables de dado ╭∩╮(-_-)╭∩╮
+semilla db 7
+dado db 0
+
+; - - - REGISTROS DE JUGADORES - - -
+
+registro_pj1 db 0
+
+registro_pj2 db 0
+
+registro_pj3 db 0
+
+registro_pj4 db 0
+
+registro_pj5 db 0
+
+;╭∩╮(-_-)╭∩╮
+act_semilla:
+    mov ah, 0 
+    int 1Ah
+    mov al, dl 
+    mov [semilla], al
+    ret
+
+;╭∩╮(-_-)╭∩╮
+generar_dado:
+    call act_semilla;actualiza la semilla usando los ticks de este momento instante espacio temporal actual inmediato ahora ya jsjshs
+    mov al, [semilla] ;carga semilla en al
+    mov ah, 0 ;limpia ah para la división
+    mov bl, 6 ;mueve 6 a bl
+    div bl ;divide ax entre bl
+    mov al, ah ; pone el residuo en al
+    add al, 1 ;suma 1 a al para tener un valor del 1 al 6
+    mov [dado], al ; mueve al a la variable dado
+    ret
+
+; - - - COMPARACIÓN DE JUGADORES - - -
 
 main:
-    ; Welcome message
-    push hello_world
-    call printf
-    
-    add esp, 4
+    call generar_dado
+    cmp [personajes], 0
+        je jugadores_1
 
-    ; Perform add operation
-    mov eax, 2
-    mov ebx, 3
-    add eax, ebx
+    cmp [personajes], 1
+        je jugadores_2
 
-    push eax
-    push output_msg
-    call printf
+    cmp [personajes], 2
+        je jugadores_3
 
-    add esp, 8
+    cmp [personajes], 3
+        je jugadores_4
 
-exit:
-    mov eax, 1
-    xor ebx, ebx
-    int 0x80
+    cmp [personajes], 4
+        je jugadores_5
+
+; - - - 1 JUGADORES - - -
+
+jugadores_1:
+cmp [personaje_seleccionado], 0
+je movimiento_1
+
+movimiento_1:
+mov [movimiento], [dado]
+add [registro_pj1], [dado]
+ret jugadores_1
+
+; - - - 2 JUGADORES - - -
+
+jugadores_2:
+cmp [personaje_seleccionado], 0
+    je movimiento_1
+
+cmp [personaje_seleccionado], 1
+    je movimiento_2
+
+movimiento_1:
+    mov [movimiento], [dado]
+    add [registro_pj1], [dado]
+    add [personaje_seleccionado], 1
+    ret jugadores_2
+
+movimiento_2:
+    mov [movimiento], [dado]
+    add [registro_pj2], [dado]
+    mov [personaje_seleccionado], 0
+    ret jugadores_2
+
+; - - - 3 JUGADORES - - -
+
+jugadores_3:
+cmp [personaje_seleccionado], 0
+    je movimiento_1
+
+cmp [personaje_seleccionado], 1
+    je movimiento_2
+
+cmp [personaje_seleccionado], 2
+    je movimiento_3
+
+
+movimiento_1:
+    mov [movimiento], [dado]
+    add [registro_pj1], [dado]
+    add [personaje_seleccionado], 1
+    ret jugadores_3
+
+movimiento_2:
+    mov [movimiento], [dado]
+    add [registro_pj2], [dado]
+    add [personaje_seleccionado], 1
+    ret jugadores_3
+
+movimiento_3:
+    mov [movimiento], [dado]
+    add [registro_pj3], [dado]
+    mov [personaje_seleccionado], 0
+    ret jugadores_3
+
+; - - - 4 JUGADORES - - -
+
+jugadores_4:
+cmp [personaje_seleccionado], 0
+    je movimiento_1
+
+cmp [personaje_seleccionado], 1
+    je movimiento_2
+
+cmp [personaje_seleccionado], 2
+    je movimiento_3
+
+cmp [personaje_seleccionado], 3
+    je movimiento_4
+
+
+movimiento_1:
+    mov [movimiento], [dado]
+    add [registro_pj1], [dado]
+    add [personaje_seleccionado], 1
+    ret jugadores_4
+
+movimiento_2:
+    mov [movimiento], [dado]
+    add [registro_pj2], [dado]
+    add [personaje_seleccionado], 1
+    ret jugadores_4
+
+movimiento_3:
+    mov [movimiento], [dado]
+    add [registro_pj3], [dado]
+    add [personaje_seleccionado], 1
+    ret jugadores_4
+
+movimiento_4:
+    mov [movimiento], [dado]
+    add [registro_pj4], [dado]
+    mov [personaje_seleccionado], 0
+    ret jugadores_4
+
+; - - - 5 JUGADORES - - -
+
+jugadores_5:
+cmp [personaje_seleccionado], 0
+    je movimiento_1
+
+cmp [personaje_seleccionado], 1
+    je movimiento_2
+
+cmp [personaje_seleccionado], 2
+    je movimiento_3
+
+cmp [personaje_seleccionado], 3
+    je movimiento_4
+
+cmp [personaje_seleccionado], 4
+    je movimiento_5
+
+
+movimiento_1:
+    mov [movimiento], [dado]
+    add [registro_pj1], [dado]
+    add [personaje_seleccionado], 1
+    ret jugadores_5
+
+movimiento_2:
+    mov [movimiento], [dado]
+    add [registro_pj2], [dado]
+    add [personaje_seleccionado], 1
+    ret jugadores_5
+
+movimiento_3:
+    mov [movimiento], [dado]
+    add [registro_pj3], [dado]
+    add [personaje_seleccionado], 1
+    ret jugadores_5
+
+movimiento_4:
+    mov [movimiento], [dado]
+    add [registro_pj4], [dado]
+    add [personaje_seleccionado], 1
+    ret jugadores_5
+
+movimiento_5:
+    mov [movimiento], [dado]
+    add [registro_pj5], [dado]
+    mov [personaje_seleccionado], 0
+    ret jugadores_5
+
+;╭∩╮(-_-)╭∩╮
+tablero db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+        db 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
